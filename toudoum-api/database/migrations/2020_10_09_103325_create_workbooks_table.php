2c619c7403108505902fa4d652fe16790b95af9c
Lucas Fridez <lucas.fridez@he-arc.ch>
2020-10-09 12:35:21 +0200

    Add: Workbooks migration Add a migration to create workbooks table

---
+++ toudoum-api/database/migrations/2020_10_09_103325_create_workbooks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkbooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workbooks', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 50)->index("index_workbooks_name");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workbooks');
+    }
+}
