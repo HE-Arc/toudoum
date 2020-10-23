1bd36a1072e5c288e8ce06b1bd32bd93c8e274cc
Lucas Fridez <lucas.fridez@he-arc.ch>
2020-10-23 09:25:46 +0200

    Add: first db schema (with Julien)

--- toudoum-api/database/migrations/2020_10_23_063325_create_workbooks_table.php
+++ toudoum-api/database/migrations/2020_10_23_063325_create_workbooks_table.php
@@ -16,6 +16,8 @@ class CreateWorkbooksTable extends Migration
         Schema::create('workbooks', function (Blueprint $table) {
             $table->id();
             $table->string("name", 50)->index("index_workbooks_name");
+            $table->foreignId("group_id")->references("id")->on("groups")->nullable();
+            $table->foreignId("user_id")->references("id")->on("users")->nullable();
             $table->timestamps();
         });
     }
