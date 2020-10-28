95015c15d4e931b3f6f4270fc1188a537a3a5de4
Edouard Goffinet <edouard.goffinet@he-arc.ch>
2020-10-28 19:07:00 +0100

    hotfix login

--- toudoum-api/app/Http/Controllers/Auth/AuthController.php
+++ toudoum-api/app/Http/Controllers/Auth/AuthController.php
@@ -32,7 +32,7 @@ class AuthController extends Controller
 
     public function login(LoginRequest $request)
     {
-        if (!Auth::attempt($request)) 
+        if (!Auth::attempt(['email' => $request['email'], 'password' => $request['password']]))
         {
             return $this->error('Credentials mismatch', 401);
         }
