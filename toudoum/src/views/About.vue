acc8b373f83275c5c2f7807a00b1921ad1dd3dab
Lucas Fridez <lucas.fridez@he-arc.ch>
2020-10-29 17:44:56 +0100

    Add: prettier for project web app

--- toudoum/src/views/About.vue
+++ toudoum/src/views/About.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <div v-for="t in names" :key="t.id">
-      {{ t.name }}
+    <div class="about">
+        <h1>This is an about page</h1>
+        <div v-for="t in names" :key="t.id">
+            {{ t.name }}
+        </div>
     </div>
-  </div>
 </template>
-
 
 <script lang="ts">
 import Vue from "vue";
@@ -16,27 +15,27 @@ import { ToudoumError } from "@/api/ToudoumError";
 import { ToudoumError422 } from "@/api/ToudoumError422";
 
 export default Vue.extend({
-  name: "Home",
-  components: {},
-  async mounted() {
-    try {
-      await Api.login({
-        email: "lucas.fridez@he-arc.ch",
-        password: "123123",
-      });
-      await Api.post("tasks", { name: "hello", workbood_id: "" });
-    } catch (e) {
-      if (e instanceof ToudoumError422) {
-        console.log(e.data); // Errors with sent data
-      } else if (e instanceof ToudoumError) {
-        console.log(e.message); // Error (401, 404 or 500,...)
-      }
+    name: "Home",
+    components: {},
+    async mounted() {
+        try {
+            await Api.login({
+                email: "lucas.fridez@he-arc.ch",
+                password: "123123"
+            });
+            await Api.post("tasks", { name: "hello", workbood_id: "" });
+        } catch (e) {
+            if (e instanceof ToudoumError422) {
+                console.log(e.data); // Errors with sent data
+            } else if (e instanceof ToudoumError) {
+                console.log(e.message); // Error (401, 404 or 500,...)
+            }
+        }
+    },
+    data: function () {
+        return {
+            names: [] as ITask[]
+        };
     }
-  },
-  data: function () {
-    return {
-      names: [] as ITask[],
-    };
-  },
 });
 </script>
