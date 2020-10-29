04b8cb45aa6037a6ee22616ef25ffabadcfcb967
Lucas Fridez <lucas.fridez@he-arc.ch>
2020-10-29 18:50:45 +0100

    Add: mutations and getter for User

--- toudoum/src/store/index.ts
+++ toudoum/src/store/index.ts
@@ -3,53 +3,41 @@ import Vue from 'vue'
 import { PluginObject } from 'vue/types/umd';
 
 // Vuex
-import Vuex, { Store, GetterTree, MutationTree, ActionTree } from 'vuex'
+import Vuex, { Store, GetterTree, MutationTree, ActionTree, ActionContext } from 'vuex'
+import { actions } from './Actions';
+import { getters } from './Getter';
+import { mutations } from './Mutations';
+
+// Internal properties
+import { State } from "./State"
 
 // Update Vue interface towith typedStore
 declare module "vue/types/vue" {
-  interface Vue {
-    $typedStore: Store<State>;
-  }
+    interface Vue {
+        $typedStore: Store<State>;
+    }
 }
 
 // Define typedStore property in Vuex Store
 const typedStorePlugin: PluginObject<void> = {
-  install(VueInstance: typeof Vue) {
-    Object.defineProperty(VueInstance.prototype, '$typedStore', {
-      get() {
-        return this.$store;
-      }
-    });
-  }
+    install(VueInstance: typeof Vue) {
+        Object.defineProperty(VueInstance.prototype, '$typedStore', {
+            get() {
+                return this.$store;
+            }
+        });
+    }
 };
 
 // Use Vuex and Typed Store
 Vue.use(Vuex)
 Vue.use(typedStorePlugin);
 
-const getters = {
-  getUserId(state: State) {
-    return state.userId;
-  }
-} as GetterTree<State, any>;
-
-const mutations = {
-  setUserId(state, payload) {
-    state.userId = payload;
-  }
-} as MutationTree<State>;
-
-const actions = {
-  fetchUserId(store) {
-    return 1;
-  }
-} as ActionTree<State, any>;
-
+// Export typed Store !
 export default new Vuex.Store({
-  state: new State(),
-  mutations: mutations,
-  getters: getters,
-  actions: actions,
-  modules: {}
+    state: new State(),
+    mutations: mutations,
+    getters: getters,
+    actions: actions,
 })
 
