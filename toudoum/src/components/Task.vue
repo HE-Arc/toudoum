961973fd86807987b3288d5e0467fa874ef544b7
Lucas Fridez <lucas.fridez@he-arc.ch>
2020-11-27 13:31:54 +0100

    Style: Task component

--- toudoum/src/components/Task.vue
+++ toudoum/src/components/Task.vue
@@ -2,43 +2,38 @@
 <template>
     <v-card elevation="4" class="pa-md-6 mx-lg-auto">
         <v-row>
-            <v-col cols="1">
-                <v-btn v-on:click="save" elevation="2" color="primary">Save</v-btn></v-col
-            >
-            <v-col> <v-btn v-on:click="back" elevation="2" color="primary">Back</v-btn></v-col>
-        </v-row>
-        <v-row>
-            <v-col cols="2"
-                ><v-checkbox
-                    v-on:change="clickOnCheckbox"
-                    :input-value="isTaskChecked"
-                    :label="`DO`"
-                ></v-checkbox
-            ></v-col>
-            <v-col cols="6"><v-text-field label="Title" v-model="task.name"></v-text-field></v-col>
-        </v-row>
-        <v-row>
-            <v-col cols="2">
+            <v-col sm="12" md="12" lg="6" xl="6">
+                <div class="d-flex">
+                    <v-checkbox
+                        v-on:change="clickOnCheckbox"
+                        :input-value="isTaskChecked"
+                    ></v-checkbox>
+                    <v-text-field label="Title" v-model="task.name"></v-text-field>
+                </div>
                 <v-combobox
                     v-on:change="changePriority"
                     :items="priority"
                     label="Priority"
                     :value="getPriority(task.priority)"
-                ></v-combobox
-            ></v-col>
-        </v-row>
-        <v-row no-gutters>
-            <v-date-picker v-model="task.end_date"></v-date-picker>
-        </v-row>
-
-        <v-row>
-            <v-col cols="8">
+                ></v-combobox>
                 <v-textarea
                     name="input-7-1"
+                    auto-grow
+                    rows="1"
                     label="Description"
                     v-model="task.description"
                 ></v-textarea>
             </v-col>
+            <v-col sm="12" md="12" lg="6" xl="6">
+                <v-row justify="center">
+                <v-date-picker v-model="task.end_date"></v-date-picker>
+
+                </v-row>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-btn v-on:click="save" elevation="2" color="primary">Save</v-btn>
+            <v-btn v-on:click="back" elevation="2" color="primary">Back</v-btn>
         </v-row>
     </v-card>
 </template>
@@ -86,10 +81,10 @@ export default Vue.extend({
 
         save: function () {
             Api.patch("tasks/" + this.task.id, {
-                name:this.task.name,
-                description:this.task.description,
-                priority:this.task.priority,
-                end_date:this.task.end_date
+                name: this.task.name,
+                description: this.task.description,
+                priority: this.task.priority,
+                end_date: this.task.end_date
             });
             router.go(-1);
         },
