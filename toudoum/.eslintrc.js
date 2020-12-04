cb84d439069fb4053cfc183f36bc0943840e2276
Lucas Fridez <lucas.fridez@he-arc.ch>
2020-12-04 10:45:28 +0100

    Refactor: Remove warnings

--- toudoum/.eslintrc.js
+++ toudoum/.eslintrc.js
@@ -9,6 +9,17 @@ module.exports = {
     },
     rules: {
         "@typescript-eslint/camelcase": ["error", { properties: "never" }],
+        "@typescript-eslint/naming-convention": [
+            "error",
+            {
+                selector: "interface",
+                format: ["PascalCase"],
+                custom: {
+                    regex: "^I[A-Z]",
+                    match: true
+                }
+            }
+        ],
         "@typescript-eslint/no-inferrable-types": "off",
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
